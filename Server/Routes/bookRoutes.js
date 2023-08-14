3ac34bf7e3e5025a74aa680086c9c89887e0e94b
IvanFougerat <ivan.garciafou@gmail.com>
2023-08-14 17:13:42 -0600

    Añadiendo la autenticacion basica

--- Server/Routes/bookRoutes.js
+++ Server/Routes/bookRoutes.js
@@ -2,9 +2,9 @@ const express =  require('express')
 const router = express.Router()
 const bookController = require('../Controllers/bookController')
 
-router.get('', bookController.getAllBooks)
+router.get('/', bookController.getAllBooks)
 router.get('/:id', bookController.getOneBook)
-router.post('', bookController.createBooks)
+router.post('/', bookController.createBooks)
 router.patch('/:id', bookController.updateBook)
 router.delete('/:id', bookController.deleteBook)
 
--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose')
-const app = express()
-const routes = require('./Server/Routes/router')
+
+const app = express();
+const routes = require('./Server/Routes/router');
 
 const port = 3000;
 
 app.use(express.json())
 
-app.use('/api', routes)
 
-//CONEXION A MONGO
 const mongoConnect = async () => {
     try {
         await mongoose.connect(
-            'mongodb+srv://david:davidpassword@protalento-back.2c9k6lv.mongodb.net/?retryWrites=true&w=majority'
+            'mongodb+srv://ivangarciafou:pro-talento2023@cluster0.70u6jko.mongodb.net/?retryWrites=true&w=majority'
         )
         console.log('Se realizo correctamente la conexión con Mongo DB')
     }
@@ -23,6 +22,13 @@ const mongoConnect = async () => {
     }
 }
 mongoConnect()
-app.listen(port, () => {
-    console.log(`Servidor esta en ejecucion en http://localhost:${port}`)
+
+app.get('/api', routes)
+
+app.get('/', (req, res) => {
+    res.send('Bienvenidos al Mundo!!!')
 })
+
+app.listen(port, () => {
+    console.log(`Servidor está en ejecución en http://localhost:${port}`);
+});
